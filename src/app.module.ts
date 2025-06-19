e67fcaa3195fe0fb895ad4e25d2c3257ee8cbdf2
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-06-19 13:57:07 +0600

    Controller, service & module created for book

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookController } from './book/book.controller';
+import { BookService } from './book/book.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
